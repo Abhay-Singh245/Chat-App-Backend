c396c39aae9477c854e0b3da246b11d163143eac
unknown <abhaysinghfzd2004@gmail.com>
2024-06-24 12:16:20 +0530

    commit

--- constants/config.js
+++ constants/config.js
@@ -2,7 +2,7 @@ const corsOptions = {
   origin: [
     "http://localhost:5173",
     "http://localhost:4173",
-    "https://chat-app-frontend-2wz3q2uod-abhaysingh245s-projects.vercel.app/",
+    "https://chat-app-frontend-2wz3q2uod-abhaysingh245s-projects.vercel.app",
      process.env.CLIENT_URL,
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
